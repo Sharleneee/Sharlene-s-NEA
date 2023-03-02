bb2d7f1330010978857d70ff8dcd8ba06c643f12
Sharleneee <91702453+Sharleneee@users.noreply.github.com>
2023-03-02 12:17:15 +0000

    Update index.js

--- server/index.js
+++ server/index.js
@@ -20,17 +20,31 @@ function removeClientFromClients(cID) {
     delete clients[cID];
 }
 
+function updateClientID(messageFromClient) {
+    const clientID = messageFromClient.clientID;
+    const oldClientID = messageFromClient.oldClientID;
+    const gameID = messageFromClient.gameID;
+
+    const client = games[gameID].clients.find(eachClient => eachClient.clientID === oldClientID);
+    //changing the client's clientID from the old one to the new one in games array
+    client.clientID = clientID;
+    removeClientFromClients(oldClientID);
+    //dont need to change the clientID of the corresponding client in playersInLobby list as it is automatically changed due to it being passed as reference from the games array, so it changes when game object changes
+}
+
 
 
 // functions ^^
 
 words = ["Stapler", "Desk", "Phone", "Paper", "Light", "Chair", "Notepad", "Binder", "Calculator", "Calendar", "Pens", "Pencils", "Notebook", "Book", "Chairs", "Chairs", "Thermos", "Glue", "Clipboard", "Paperclips", "Chocolate", "Secretary", "Work", "Paperwork", "Workload", "Employee", "Boredom", "Coffee", "Golf", "Laptop", "Sandcastle", "Monday", "Vanilla", "Bamboo", "Sneeze", "Scratch", "Celery", "Hammer", "Frog", "Tennis", "Pants", "Bridge", "Bubblegum", "Bucket", "Skiing", "Sledding", "Snowboarding", "Snowman", "Cream", "Waffle", "Pancakes", "Sundae", "beach", "Sunglasses", "Surfboard", "Watermelon", "Baseball", "Bat", "Ball", "Kiss", "Jellyfish", "Jelly", "Butterfly", "Spider", "Broom", "Spiderweb", "Mummy", "Candy", "Bays", "Squirrels", "Basketball", "Unicorn", "Newspaper", "Girl", "Boy"];
 const http = require("http");
+const { start } = require("repl");
 // const { getuid } = require("process");
 const WebSocketServer = require("websocket").server;
 let connection = null;
 let clients = {};
 let games = {};
+let clock = null;
 
 const httpserver = http.createServer()
 
@@ -64,21 +78,8 @@ wss.on("request", request => {
         }
         const messageFromClient = JSON.parse(message.utf8Data);
 
-        // if the data received is a message,
-        if (messageFromClient.method === "message") {  
-            const gameID = messageFromClient.gameID;   
-            const clientID = messageFromClient.clientID;
 
-            games[gameID].clients.forEach(eachClient => { // it will be sent to all the clients apart from the one that sent it
-                const client = clients[eachClient.clientID];
-                if (client.connection !== clients[clientID].connection) { 
-                    client.connection.send(JSON.stringify({
-                        type: "message",
-                        messageContent: messageFromClient.messageContent
-                    }));
-                }
-            })
-        }
+
 
         // homepage / index ///////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////////////////
@@ -157,49 +158,10 @@ wss.on("request", request => {
         /////////////////////////////////////////////////////////////////////////////////////
 
         if (messageFromClient.method === "updateClientID") {
-            const clientID = messageFromClient.clientID;
-            const oldClientID = messageFromClient.oldClientID;
-            const gameID = messageFromClient.gameID;
-
-            const client = games[gameID].clients.find(eachClient => eachClient.clientID === oldClientID);
-            //changing the client's clientID from the old one to the new one in games array
-            client.clientID = clientID;
-            removeClientFromClients(oldClientID);
+            updateClientID(messageFromClient);
         }
 
-        if (messageFromClient.method === "updateClientIDGamepage") {
-            const clientID = messageFromClient.clientID;
-            const oldClientID = messageFromClient.oldClientID;
-            const gameID = messageFromClient.gameID;
-            let isDrawer = false;
-            let threeWords = [];
 
-            let client = games[gameID].clients.find(eachClient => eachClient.clientID === oldClientID);
-            //changing the client's clientID from the old one to the new one
-            client.clientID = clientID;
-            removeClientFromClients(oldClientID);
-
-            //dont need to change the clientID of the corresponding client in playersInLobby list as it is automatically changed due to it being passed as reference from the games array, so it changes when game object changes
-
-            // sends messsage to initialise game after client's clientID updated
-            // assuming all players in lobby continue to gamepage
-
-            if (games[gameID].playersInLobby[0].clientID === clientID) {
-                isDrawer = true;
-                threeWords = genThreeWords(gameID, games[gameID].gameSettings.listWordsAdded);
-            }
-
-            clients[clientID].connection.send(JSON.stringify({
-                method: "initialiseGame",
-                gameSettings: {
-                    numRounds: games[gameID].gameSettings.numRounds,
-                    roundLength: games[gameID].gameSettings.roundLength,
-                },
-                isDrawer: isDrawer,
-                threeWords: threeWords
-
-            }));
-        }
 
         if (messageFromClient.method === "updatePlayersInLobby") {
 
@@ -217,7 +179,7 @@ wss.on("request", request => {
 
             }
             else if (messageFromClient.event === "exit") {
-                if(games[gameID].gameStarted){
+                if (games[gameID].gameStarted) {
                     return;
                 }
                 //assuming exit method is only used when there are more than 0 players in playersInLobby list.
@@ -225,7 +187,7 @@ wss.on("request", request => {
                 //deleting client from players list:
                 games[gameID].playersInLobby.splice(indexInPlayersList, 1);
                 //deleting client from game clients list:
-                removeClientFromGame(gameID, clientID); 
+                removeClientFromGame(gameID, clientID);
 
             }
             // sends the updated playersInLobby list to everyon
@@ -300,10 +262,15 @@ wss.on("request", request => {
                 const numPlayersInLobby = games[gameID].playersInLobby.length; // when people leave, this number goes down so the last person isn't able to "play the game" so I am assigning it to a const
                 const client = clients[eachClient.clientID];
                 const readyToStart = games[gameID].clients.length > 1;
+                let reason = ""
+                if (!readyToStart) {
+                    reason = "Cannot start game as there is not enough people to start the game (need more than one player).";
+                }
+
                 client.connection.send(JSON.stringify({
                     method: "startGame",
                     readyToStart: readyToStart,
-                    reason: "Cannot start game as there is not enough people to start the game (need more than one player)."
+                    reason: reason
                 }));
             })
 
@@ -311,24 +278,33 @@ wss.on("request", request => {
 
         // GAME PAGE ///////////////////////////////////////////////////////////////////////
         /////////////////////////////////////////////////////////////////////////////////////
+        if (messageFromClient.method === "updateClientIDGamepage") {
+            updateClientID(messageFromClient);
+            initialiseGame(messageFromClient.gameID, messageFromClient.clientID);
+        }
 
 
-        if (messageFromClient.method === "choose3Words") {
+
+        if (messageFromClient.method === "wordChosen") {
 
 
 
         }
 
+        // if the data received is a message,
         if (messageFromClient.method === "message") {
             const gameID = messageFromClient.gameID;
             const clientID = messageFromClient.clientID;
-            games[gameID].clients.forEach(eachClient => {
-                const client = clients[clientID];
+            const messageSentBy = games[gameID].clients.find(eachClient => eachClient.clientID === clientID).playerName;
+
+            games[gameID].clients.forEach(eachClient => { // it will be sent to all the clients apart from the one that sent it
+                const client = clients[eachClient.clientID];
                 client.connection.send(JSON.stringify({
                     method: "message",
+                    messageSentBy: messageSentBy,
                     messageContent: messageFromClient.messageContent
                 }));
-            });
+            })
         }
 
 
@@ -355,6 +331,8 @@ wss.on("request", request => {
 
 
 ////////// GAMEPAGE ////////////////////////
+
+
 
 function getRandomWord(listWordsAdded, numWordsAdded) {
     //generate a random number from 
@@ -388,10 +366,163 @@ function genThreeWords(gameID, listWordsAdded) {
             threeWords.push(wordGenerated);
         }
         // repeat until 3 words can be pushed into threeWords list
-        else{
+        else {
             i -= 1;
         }
     }
     return threeWords;
 }
 
+function secondsFromNow(secondsToAdd) {
+    let now = Date().getTime();
+    return now.setSeconds(now.getSeconds() + secondsToAdd);
+}
+
+function startPlaying(gameID, clientID, wordChosen) {
+
+    if (games[gameID].currentRound === undefined) {
+        // adds a current round property to game and initalises it to 1
+        games[gameID].currentRound = 1;
+    }
+    // increments current round property
+    games[gameID].currentRound += 1;
+
+    // adds a round finish time property to game
+    const roundLength = games[gameID].gameSettings.roundLength;
+    games[gameID].finishRoundBy = secondsFromNow(roundLength);
+
+    // starts the round clock
+    clock = setInterval(checkTime(games[gameID].finishRoundBy, "round", gameID, clientID), 1000);
+
+    // sends to all game clients to start the round
+    games[gameID].clients.forEach(eachClient => {
+        const client = clients[eachClient.clientID];
+        client.connection.send(JSON.stringify({
+            method: "startPlaying",
+            wordChosen: wordChosen,
+            roundFinishTime: games[gameID].finishRoundBy
+        }));
+    });
+}
+
+function endGame(gameID) {
+    // sets game finish property of game to true and sends message to everyone that game is finished along with the game end state
+}
+
+function checkIfGameOver(gameID) {
+    //checks if number of rounds completed and if drawer pointer is at end of playersInLobby list
+    if ((games[gameID].currentRound === games[gameID].gameSettings.numRounds) && (games[gameID].drawerPointer === (games[gameID].playersInLobby.length - 1))) {
+        return true;
+    }
+    return false;
+}
+
+function incrementDrawerPointer(gameID){
+    // circular array
+    // checks if the drawer pointer is at the end of the list and sends it to the start
+    if (games[gameID].drawerPointer === (games[gameID].playersInLobby.length - 1)) {
+        games[gameID].drawerPointer = 0;
+    }
+    else {
+        games[gameID].drawerPointer += 1;
+    }
+}
+
+function roundOver(gameID, clientID) {
+    // checks if game over and ends game if it is
+    if (checkIfGameOver(gameID)) {
+        endGame(gameID);
+        return;
+    }
+
+    incrementDrawerPointer(gameID);
+
+    const nextDrawer = games[gameID].playersInLobby[games[gameID].drawerPointer];
+
+    startRound(gameID, clientID);
+
+    // sends to all game clients  IDK IF WE NEED BELOW
+    games[gameID].clients.forEach(eachClient => {
+        const client = clients[eachClient.clientID];
+        client.connection.send(JSON.stringify({
+            method: "startRound",
+            nextDrawer: nextDrawer
+        }));
+    });
+}
+
+function checkTime(timeToCheck, typeOfTime, gameID, clientID) {
+    const now = new Date().getTime();
+    const difference = timeToCheck - now; // differrence is in milliseconds
+    const secondsLeft = Math.floor((distance % (1000 * 60)) / 1000);
+    // when the timer is up
+    if (secondsLeft <= 0) {
+        clearInterval(clock);
+        if (typeOfTime === "chooseWord") {
+            // as the drawer has chosen the word, the game can now start
+            startPlaying(gameID, clientID);
+        }
+        else if (typeOfTime === "round") {
+            roundOver(gameID, clientID);
+        }
+    }
+}
+
+function checkIfPlayerIsDrawer(gameID, clientID){
+    // if the client is not a drawer, this is indicated by the empty threeWordslist
+    let threeWords = [];
+
+    if (games[gameID].playersInLobby[0].clientID === clientID) {
+        threeWords = genThreeWords(gameID, games[gameID].gameSettings.listWordsAdded);
+    }
+    return threeWords;
+}
+
+function startRound(gameID, clientID){
+    // adds the chooseWordBy time to the game object
+    const secondsToChooseWord = 10;
+    games[gameID].chooseWordBy = secondsFromNow(secondsToChooseWord);
+
+    const typeOfTime = "chooseWord";
+    clock = setInterval(checkTime(timeToCheck, typeOfTime, gameID, clientID), 1000); // checks per second
+
+    //////////////////////////////////////
+    // assigning the variables to send to client:
+
+    let isDrawer = false;
+    const threeWords = checkIfPlayerIsDrawer(gameID, clientID);
+    if(threeWords.length === 3){
+        isDrawer = true;
+    }
+
+    // empty object will be sent if it is not the first round 
+    // as the player sets these settings to a variable on the first round so they can access it from there onwards
+    let gameSettingsToSend = {};
+    if((games[gameID].currentRound === 1)&&(games[gameID].drawerPointer === 0)){
+        gameSettingsToSend = {
+            numRounds: games[gameID].gameSettings.numRounds,
+            roundLength: games[gameID].gameSettings.roundLength,
+        };
+    }
+
+    clients[clientID].connection.send(JSON.stringify({
+        method: "startRound",
+        gameSettings: gameSettingsToSend,
+        isDrawer: isDrawer,
+        threeWords: threeWords,
+        chooseWordBy: games[gameID].chooseWordBy
+
+    }));
+}
+
+function initialiseGame(gameID, clientID) {
+    // sends messsage to initialise game and start the first round after client's clientID updated
+    
+    // adds a game drawer pointer to point to a player in playersInLobby
+    games[gameID].drawerPointer = 0;
+
+    // adds a gameFinished property to game and initialises it to false
+    games[gameID].gameFinished = false;
+
+    startRound(gameID, clientID);
+}
